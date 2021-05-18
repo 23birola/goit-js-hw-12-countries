ff46d9257b8d8c16d68f6ab8c5e50c972740a3de
23birola <23birola@gmail.com>
2021-05-18 23:03:37 +0300

    fixed code

--- src/index.js
+++ src/index.js
@@ -1,44 +1,27 @@
 import './sass/main.scss';
 import debounce from 'lodash.debounce';
-import { alert, defaultModules } from '@pnotify/core';
-import * as PNotifyMobile from '@pnotify/mobile';
-import countryCardTemplate from './templates/country-card.hbs';
-import countriesListTemplate from './templates/countries-list.hbs';
+import fetchCountries from './js/fetchCountries';
 
-// import fetchCountries from './js/fetchCountries'
-const BASE_URL = 'https://restcountries.eu'
 const refs = {
   searchCountry: document.querySelector('#country'),
   countryContainer: document.querySelector('.country')
 };
 
-refs.searchCountry.addEventListener('input', debounce(fetchCountries, 1000));
+refs.searchCountry.addEventListener('input', debounce(onSearch, 1000));
 
-function fetchCountries(e) {
+function onSearch(e) {
+  const searchQuery = e.target.value;
   refs.countryContainer.innerHTML = "";
-  let countryQuery = e.target.value;
-  fetch(`${BASE_URL}/rest/v2/name/${countryQuery}`)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(error);
-  })
-    .then(data => {
-    if (data.length > 10) {
-        defaultModules.set(PNotifyMobile, {});
-        alert({
-            text: 'Too many matches found. Please enter a more specific query'
-        });
-        throw new Error(error);
-    }
-    if (data.length > 1) {
-      return countriesListTemplate(data);
-    }
-    return countryCardTemplate(data);
-  })
-  .then(markup => { return refs.countryContainer.innerHTML = markup;})
-  .catch(error => {
-    console.log(error);
-  });
+
+  fetchCountries(searchQuery) 
+  .then(renderResult)
+  .catch(onFetchError);
+}
+
+function renderResult(markup) {
+  return refs.countryContainer.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  console.log(error);
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,29 @@
-export default function fetchCountries(searchQuery){
-    
+import countryCardTemplate from '../templates/country-card.hbs';
+import countriesListTemplate from '../templates/countries-list.hbs';
+import { alert, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+
+const BASE_URL = 'https://restcountries.eu';
+
+export default function fetchCountries(searchQuery) {
+ return fetch(`${BASE_URL}/rest/v2/name/${searchQuery}`)
+     .then(response => { 
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(error);
+     })
+     .then(data => {
+    if (data.length > 10) {
+        defaultModules.set(PNotifyMobile, {});
+        alert({
+            text: 'Too many matches found. Please enter a more specific query'
+        });
+        throw new Error(error);
+    }
+    if (data.length > 1) {
+      return countriesListTemplate(data);
+    }
+    return countryCardTemplate(data);
+  })   
 }
